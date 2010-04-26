5e6cb7f156b92eef14c942436e4d526950554cd1
ferencek <>
2010-04-26 19:31:30 +0000

    *** empty log message ***

---
+++ test/rootTupleMaker_DATA_cfg.py
@@ -0,0 +1,168 @@
+# Starting with a skeleton process which gets imported with the following line
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+
+from PhysicsTools.PatAlgos.tools.coreTools import *
+
+############## IMPORTANT ########################################
+# If you run over many samples and you save the log, remember to reduce
+# the size of the output by prescaling the report of the event number
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
+process.MessageLogger.cerr.default.limit = 100
+#################################################################
+
+# Load RootTupleMakerV2 modules
+process.load('Leptoquarks.RootTupleMakerV2.Ntuple_cff')
+
+# Output ROOT file
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('RootTupleMakerV2_output_DATA.root')
+)
+
+# Global tag (make sure it always matches with the global tag used to reconstruct input files)
+process.GlobalTag.globaltag = 'GR_R_35X_V7A::All'
+
+# Events to process
+process.maxEvents.input = 1000
+
+# Options and Output Report
+process.options.wantSummary = True
+
+# Input files
+process.source.fileNames = [
+    '/store/data/Commissioning10/MinimumBias/RECO/Apr20ReReco-v1/0165/96ED8CE5-EA4C-DF11-AC05-0018F3D096F8.root'
+]
+
+# Turn off MC matching for the process
+removeMCMatching(process, ['All'])
+
+# Add tcMET and pfMET
+from PhysicsTools.PatAlgos.tools.metTools import *
+addTcMET(process, 'TC')
+addPfMET(process, 'PF')
+
+# Get the 7 TeV GeV jet corrections
+from PhysicsTools.PatAlgos.tools.jetTools import *
+switchJECSet( process, "Summer09_7TeV_ReReco332")
+
+# Add PF jets
+addJetCollection(process,cms.InputTag('ak5PFJets'),
+    'AK5', 'PF',
+    doJTA        = False,
+    doBTagging   = False,
+    jetCorrLabel = ('AK5','PF'),
+    doType1MET   = False,
+    doL1Cleaning = False,
+    doL1Counters = False,
+    genJetCollection=cms.InputTag("ak5GenJets"),
+    doJetID      = False
+)
+
+from PhysicsTools.PatAlgos.tools.cmsswVersionTools import *
+# Run ak5 gen jets
+#run33xOnReRecoMC( process, "ak5GenJets")
+
+# Switch on PAT trigger
+#from PhysicsTools.PatAlgos.tools.trigTools import switchOnTrigger
+#switchOnTrigger( process )
+
+# Restrict input to AOD
+#restrictInputToAOD(process, ['All'])
+
+# HEEPify PAT electrons
+from SHarper.HEEPAnalyzer.HEEPSelectionCuts_cfi import *
+process.heepPatElectrons = cms.EDProducer("HEEPAttStatusToPAT",
+    eleLabel = cms.InputTag("patElectrons"),
+    barrelCuts = cms.PSet(heepBarrelCuts),
+    endcapCuts = cms.PSet(heepEndcapCuts)
+)
+
+# Add 'heepPatElectrons' in the right place and point 'selectedLayer1Electrons' to them
+process.patDefaultSequence.replace( process.patElectrons, process.patElectrons*process.heepPatElectrons )
+process.selectedPatElectrons.src = cms.InputTag("heepPatElectrons")
+
+# Electron and jet cleaning deltaR parameters
+process.cleanPatElectrons.checkOverlaps.muons.deltaR = 0.3
+process.cleanPatJets.checkOverlaps.muons.deltaR = 0.5
+process.cleanPatJets.checkOverlaps.electrons.deltaR = 0.5
+
+# PhysicsDeclared filter
+process.load('HLTrigger.special.hltPhysicsDeclared_cfi')
+process.hltPhysicsDeclared.L1GtReadoutRecordTag = 'gtDigis'
+
+# BPTX & BSC triggers filter
+process.load('L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMaskTechTrigConfig_cff')
+process.load('HLTrigger/HLTfilters/hltLevel1GTSeed_cfi')
+process.hltLevel1GTSeed.L1TechTriggerSeeding = cms.bool(True)
+process.hltLevel1GTSeed.L1SeedsLogicalExpression = cms.string('0 AND (40 OR 41) AND NOT (36 OR 37 OR 38 OR 39)')
+
+# Primary vertex filter
+process.primaryVertexFilter = cms.EDFilter("VertexSelector",
+    src = cms.InputTag("offlinePrimaryVertices"),
+    cut = cms.string("!isFake && ndof > 4 && abs(z) <= 15 && position.Rho <= 2"),
+    filter = cms.bool(True)
+)
+
+# Scraping filter
+process.scrapingVeto = cms.EDFilter("FilterOutScraping",
+    applyfilter = cms.untracked.bool(True),
+    debugOn = cms.untracked.bool(False),
+    numtrack = cms.untracked.uint32(10),
+    thresh = cms.untracked.double(0.2)
+)
+
+# Skim definition
+process.load("Leptoquarks.LeptonJetFilter.leptonjetfilter_cfi")
+process.LJFilter.muLabel = 'muons'
+process.LJFilter.elecLabel = 'gsfElectrons'
+process.LJFilter.jetLabel = 'ak5CaloJets'
+process.LJFilter.muPT = 10.
+process.LJFilter.elecPT = 30.
+
+# RootTupleMakerV2 tree
+process.rootTupleTree = cms.EDAnalyzer("RootTupleMakerV2_Tree",
+    outputCommands = cms.untracked.vstring(
+        'drop *',
+        'keep *_rootTupleEvent_*_*',
+        'keep *_rootTupleEventSelection_*_*',
+        'keep *_rootTupleCaloJets_*_*',
+        'keep *_rootTuplePFJets_*_*',
+        'keep *_rootTupleElectrons_*_*',
+        'keep *_rootTupleCaloMET_*_*',
+        'keep *_rootTupleTCMET_*_*',
+        'keep *_rootTuplePFMET_*_*',
+        'keep *_rootTupleMuons_*_*',
+        'keep *_rootTupleSuperClusters_*_*',
+        'keep *_rootTupleTrigger_*_*'
+    )
+)
+
+# Path definition
+process.p = cms.Path(
+    process.hltPhysicsDeclared*
+    process.hltLevel1GTSeed*
+    process.primaryVertexFilter*
+    process.scrapingVeto*
+    process.LJFilter*
+    process.patDefaultSequence*
+    (
+    process.rootTupleEvent+
+    process.rootTupleEventSelection+
+    process.rootTupleCaloJets+
+    process.rootTuplePFJets+
+    process.rootTupleElectrons+
+    process.rootTupleCaloMET+
+    process.rootTupleTCMET+
+    process.rootTuplePFMET+
+    process.rootTupleMuons+
+    process.rootTupleSuperClusters+
+    process.rootTupleTrigger
+    )
+    *process.rootTupleTree
+)
+
+# Delete predefined Endpath (needed for running with CRAB)
+del process.out
+del process.outpath
+
+# Schedule definition
+process.schedule = cms.Schedule(process.p)

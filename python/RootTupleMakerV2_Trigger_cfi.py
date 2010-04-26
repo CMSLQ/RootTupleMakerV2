9fa5d0a0fd1047ff2834fd10314e53819ee105ab
ferencek <>
2010-04-26 16:09:22 +0000

    *** empty log message ***

--- python/RootTupleMakerV2_Trigger_cfi.py
+++ python/RootTupleMakerV2_Trigger_cfi.py
@@ -3,5 +3,5 @@ import FWCore.ParameterSet.Config as cms
 rootTupleTrigger = cms.EDProducer("RootTupleMakerV2_Trigger",
     L1InputTag  = cms.InputTag('gtDigis'),
     HLTInputTag = cms.InputTag('TriggerResults','','HLT'),
-    HLTPathsOfInterest = cms.vstring()
+    HLTPathsOfInterest = cms.vstring('HLT_Photon15_L1R','HLT_Mu9')
 )

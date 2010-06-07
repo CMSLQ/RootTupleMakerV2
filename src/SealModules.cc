8f6e9478fb1ebf123dfc83d95b98804426eb8c92
ferencek <>
2010-06-07 21:18:39 +0000

    replaced DEFINE_ANOTHER_FWK_MODULE() with DEFINE_FWK_MODULE()

--- src/SealModules.cc
+++ src/SealModules.cc
@@ -1,8 +1,5 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
-DEFINE_SEAL_MODULE();
-
 #include "Leptoquarks/RootTupleMakerV2/interface/RootTupleMakerV2_Tree.h"
 #include "Leptoquarks/RootTupleMakerV2/interface/RootTupleMakerV2_Event.h"
 #include "Leptoquarks/RootTupleMakerV2/interface/RootTupleMakerV2_EventSelection.h"
@@ -17,16 +14,16 @@ DEFINE_SEAL_MODULE();
 #include "Leptoquarks/RootTupleMakerV2/interface/RootTupleMakerV2_GenParticles.h"
 #include "Leptoquarks/RootTupleMakerV2/interface/RootTupleMakerV2_GenJets.h"
 
-DEFINE_ANOTHER_FWK_MODULE(RootTupleMakerV2_Tree);
-DEFINE_ANOTHER_FWK_MODULE(RootTupleMakerV2_Event);
-DEFINE_ANOTHER_FWK_MODULE(RootTupleMakerV2_EventSelection);
-DEFINE_ANOTHER_FWK_MODULE(RootTupleMakerV2_CaloJets);
-DEFINE_ANOTHER_FWK_MODULE(RootTupleMakerV2_PFJets);
-DEFINE_ANOTHER_FWK_MODULE(RootTupleMakerV2_Electrons);
-DEFINE_ANOTHER_FWK_MODULE(RootTupleMakerV2_MET);
-DEFINE_ANOTHER_FWK_MODULE(RootTupleMakerV2_Muons);
-DEFINE_ANOTHER_FWK_MODULE(RootTupleMakerV2_SuperClusters);
-DEFINE_ANOTHER_FWK_MODULE(RootTupleMakerV2_Trigger);
-DEFINE_ANOTHER_FWK_MODULE(RootTupleMakerV2_GenEventInfo);
-DEFINE_ANOTHER_FWK_MODULE(RootTupleMakerV2_GenParticles);
-DEFINE_ANOTHER_FWK_MODULE(RootTupleMakerV2_GenJets);
+DEFINE_FWK_MODULE(RootTupleMakerV2_Tree);
+DEFINE_FWK_MODULE(RootTupleMakerV2_Event);
+DEFINE_FWK_MODULE(RootTupleMakerV2_EventSelection);
+DEFINE_FWK_MODULE(RootTupleMakerV2_CaloJets);
+DEFINE_FWK_MODULE(RootTupleMakerV2_PFJets);
+DEFINE_FWK_MODULE(RootTupleMakerV2_Electrons);
+DEFINE_FWK_MODULE(RootTupleMakerV2_MET);
+DEFINE_FWK_MODULE(RootTupleMakerV2_Muons);
+DEFINE_FWK_MODULE(RootTupleMakerV2_SuperClusters);
+DEFINE_FWK_MODULE(RootTupleMakerV2_Trigger);
+DEFINE_FWK_MODULE(RootTupleMakerV2_GenEventInfo);
+DEFINE_FWK_MODULE(RootTupleMakerV2_GenParticles);
+DEFINE_FWK_MODULE(RootTupleMakerV2_GenJets);

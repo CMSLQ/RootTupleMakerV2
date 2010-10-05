14839cacd7e55ed293801b7037b39367ced4ee4b
lockner <>
2010-10-05 01:16:16 +0000

    changed to use corrected sc collections

--- python/RootTupleMakerV2_SuperClusters_cfi.py
+++ python/RootTupleMakerV2_SuperClusters_cfi.py
@@ -1,8 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 rootTupleSuperClusters = cms.EDProducer("RootTupleMakerV2_SuperClusters",
-    EBInputTag = cms.InputTag('hybridSuperClusters'),
-    EEInputTag = cms.InputTag('multi5x5SuperClustersWithPreshower'),
+#    EBInputTag = cms.InputTag('hybridSuperClusters'),
+#    EEInputTag = cms.InputTag('multi5x5SuperClustersWithPreshower'),
+    EBInputTag = cms.InputTag('correctedHybridSuperClusters'),
+    EEInputTag = cms.InputTag('correctedMulti5x5SuperClustersWithPreshower'),
     Prefix = cms.string('SuperCluster'),
     Suffix = cms.string(''),
     EBMaxSize = cms.uint32(30),

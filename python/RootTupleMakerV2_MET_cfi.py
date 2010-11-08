85a0d963246530cb9fb3871a5eb683b7fc6271f1
eberry <>
2010-11-08 18:23:29 +0000

    Updated to allow user to decide whether to include uncorrected MET.  Also includes Type1 MET corrections.

--- python/RootTupleMakerV2_MET_cfi.py
+++ python/RootTupleMakerV2_MET_cfi.py
@@ -3,17 +3,34 @@ import FWCore.ParameterSet.Config as cms
 rootTupleCaloMET = cms.EDProducer("RootTupleMakerV2_MET",
     InputTag = cms.InputTag('patMETs'),
     Prefix = cms.string('Calo'),
-    Suffix = cms.string('')
+    Suffix = cms.string(''),
+    StoreUncorrectedMET = cms.bool ( True )                                   
+)
+
+rootTupleCaloMETType1Cor = cms.EDProducer ("RootTupleMakerV2_MET",
+    InputTag = cms.InputTag('patMETsCaloType1Cor'),
+    Prefix = cms.string ('Calo'),
+    Suffix = cms.string (''),                   
+    StoreUncorrectedMET = cms.bool ( False )  
 )
 
 rootTupleTCMET = cms.EDProducer("RootTupleMakerV2_MET",
     InputTag = cms.InputTag('patMETsTC'),
     Prefix = cms.string('TC'),
-    Suffix = cms.string('')
+    Suffix = cms.string(''),
+    StoreUncorrectedMET = cms.bool ( True )                                   
 )
 
 rootTuplePFMET = cms.EDProducer("RootTupleMakerV2_MET",
     InputTag = cms.InputTag('patMETsPF'),
     Prefix = cms.string('PF'),
-    Suffix = cms.string('')
+    Suffix = cms.string(''),
+    StoreUncorrectedMET = cms.bool ( True )                                   
+)
+
+rootTuplePFMETType1Cor = cms.EDProducer("RootTupleMakerV2_MET",
+    InputTag = cms.InputTag('patMETsPFType1Cor'),
+    Prefix = cms.string('PF'),
+    Suffix = cms.string('Type1Cor'),
+    StoreUncorrectedMET = cms.bool ( False )                                   
 )

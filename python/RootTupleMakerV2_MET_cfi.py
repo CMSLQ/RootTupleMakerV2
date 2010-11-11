59de8ce511ac5844004769d8f477fe14f7f44f5a
ferencek <>
2010-11-11 02:09:08 +0000

    cleaned up cfi file

--- python/RootTupleMakerV2_MET_cfi.py
+++ python/RootTupleMakerV2_MET_cfi.py
@@ -4,33 +4,26 @@ rootTupleCaloMET = cms.EDProducer("RootTupleMakerV2_MET",
     InputTag = cms.InputTag('patMETs'),
     Prefix = cms.string('Calo'),
     Suffix = cms.string(''),
-    StoreUncorrectedMET = cms.bool ( True )                                   
-)
-
-rootTupleCaloMETType1Cor = cms.EDProducer ("RootTupleMakerV2_MET",
-    InputTag = cms.InputTag('patMETsCaloType1Cor'),
-    Prefix = cms.string ('Calo'),
-    Suffix = cms.string (''),                   
-    StoreUncorrectedMET = cms.bool ( False )  
+    StoreUncorrectedMET = cms.bool(True)
 )
 
 rootTupleTCMET = cms.EDProducer("RootTupleMakerV2_MET",
     InputTag = cms.InputTag('patMETsTC'),
     Prefix = cms.string('TC'),
     Suffix = cms.string(''),
-    StoreUncorrectedMET = cms.bool ( True )                                   
+    StoreUncorrectedMET = cms.bool(False)
 )
 
 rootTuplePFMET = cms.EDProducer("RootTupleMakerV2_MET",
     InputTag = cms.InputTag('patMETsPF'),
     Prefix = cms.string('PF'),
     Suffix = cms.string(''),
-    StoreUncorrectedMET = cms.bool ( True )                                   
+    StoreUncorrectedMET = cms.bool(False)
 )
 
 rootTuplePFMETType1Cor = cms.EDProducer("RootTupleMakerV2_MET",
     InputTag = cms.InputTag('patMETsPFType1Cor'),
     Prefix = cms.string('PF'),
     Suffix = cms.string('Type1Cor'),
-    StoreUncorrectedMET = cms.bool ( False )                                   
+    StoreUncorrectedMET = cms.bool(False)
 )

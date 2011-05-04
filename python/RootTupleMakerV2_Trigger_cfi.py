fdac44e3dd54fc5d678477ab28c296158d01c691
santanas <>
2011-05-04 11:02:50 +0000

    double photon triggers added

--- python/RootTupleMakerV2_Trigger_cfi.py
+++ python/RootTupleMakerV2_Trigger_cfi.py
@@ -26,27 +26,44 @@ rootTupleTrigger = cms.EDProducer("RootTupleMakerV2_Trigger",
                                      'HLT_Ele32_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v2', #8
                                      'HLT_Ele45_CaloIdVT_TrkIdT_v3', #9
                                      'HLT_Ele90_NoSpikeFilter_v3', #10
+
                                      ## Single Photons ##
                                      # from /cdaq/physics/Run2011/5e32/v4.2/HLT/V1
-                                     'HLT_Photon30_CaloIdVL_IsoL_v1', #
-                                     'HLT_Photon30_CaloIdVL_v1', #
-                                     'HLT_Photon75_CaloIdVL_IsoL_v1', #
-                                     'HLT_Photon75_CaloIdVL_v1', #
+                                     'HLT_Photon30_CaloIdVL_IsoL_v1', #11
+                                     'HLT_Photon30_CaloIdVL_v1', #12
+                                     'HLT_Photon75_CaloIdVL_IsoL_v1', #13
+                                     'HLT_Photon75_CaloIdVL_v1', #14
                                      # from /cdaq/physics/Run2011/5e32/v6.1/HLT/V1
-                                     'HLT_Photon20_CaloIdVL_IsoL_v1', #
-                                     'HLT_Photon30_CaloIdVL_IsoL_v2', #
-                                     'HLT_Photon30_CaloIdVL_v2', #
-                                     'HLT_Photon50_CaloIdVL_IsoL_v1', #
-                                     'HLT_Photon75_CaloIdVL_IsoL_v2', #
-                                     'HLT_Photon75_CaloIdVL_v2', #
+                                     'HLT_Photon20_CaloIdVL_IsoL_v1', #15
+                                     'HLT_Photon30_CaloIdVL_IsoL_v2', #16
+                                     'HLT_Photon30_CaloIdVL_v2', #17
+                                     'HLT_Photon50_CaloIdVL_IsoL_v1', #18
+                                     'HLT_Photon75_CaloIdVL_IsoL_v2', #19
+                                     'HLT_Photon75_CaloIdVL_v2', #20
                                      # from /cdaq/physics/Run2011/5e32/v8.1/HLT/V5
-                                     'HLT_Photon20_CaloIdVL_IsoL_v2', #
-                                     'HLT_Photon30_CaloIdVL_IsoL_v3', #
-                                     'HLT_Photon30_CaloIdVL_v3', #
-                                     'HLT_Photon50_CaloIdVL_IsoL_v2', #
-                                     'HLT_Photon75_CaloIdVL_IsoL_v3', #
-                                     'HLT_Photon75_CaloIdVL_v3', #
+                                     'HLT_Photon20_CaloIdVL_IsoL_v2', #21
+                                     'HLT_Photon30_CaloIdVL_IsoL_v3', #22
+                                     'HLT_Photon30_CaloIdVL_v3', #23
+                                     'HLT_Photon50_CaloIdVL_IsoL_v2', #24
+                                     'HLT_Photon75_CaloIdVL_IsoL_v3', #25
+                                     'HLT_Photon75_CaloIdVL_v3', #26
+                                     
                                      ## Double Photons ##
+                                     # from /cdaq/physics/Run2011/5e32/v4.2/HLT/V1
+                                     'HLT_DoublePhoton33_v1', #
+                                     'HLT_Photon26_CaloIdL_IsoVL_Photon18_v1', #
+                                     'HLT_Photon26_IsoVL_Photon18_v1', #
+                                     'HLT_Photon32_CaloIdL_Photon26_CaloIdL_v1', #
+                                     # from /cdaq/physics/Run2011/5e32/v6.1/HLT/V1
+                                     'HLT_DoublePhoton33_v2', #
+                                     'HLT_Photon26_CaloIdL_IsoVL_Photon18_v2', #
+                                     'HLT_Photon26_IsoVL_Photon18_v2', #
+                                     'HLT_Photon32_CaloIdL_Photon26_CaloIdL_v2', #
+                                     # from /cdaq/physics/Run2011/5e32/v8.1/HLT/V5
+                                     'HLT_DoublePhoton33_v3', #
+                                     'HLT_Photon26_CaloIdL_IsoVL_Photon18_v3', #
+                                     'HLT_Photon26_IsoVL_Photon18_IsoVL_v3', #
+                                     'HLT_Photon32_CaloIdL_Photon26_CaloIdL_v3', #
 
                                      ## Muons ##                                     
 

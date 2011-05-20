228ae653d447ac6f793044628df132ed473c95bc
darinb <>
2011-05-20 16:59:55 +0000

    RootTupleMakerV2_Muons_cfi.py

--- python/RootTupleMakerV2_Muons_cfi.py
+++ python/RootTupleMakerV2_Muons_cfi.py
@@ -7,7 +7,7 @@ rootTupleMuons = cms.EDProducer("RootTupleMakerV2_Muons",
     MaxSize = cms.uint32(10),
     MuonIso = cms.double(0.05),
     MuonID = cms.string('GlobalMuonPromptTight'),
-    BeamSpotCorr = cms.bool(False),
+    BeamSpotCorr = cms.bool(True),
     UseCocktailRefits = cms.bool(False),
     VertexInputTag = cms.InputTag('offlinePrimaryVertices')
 )

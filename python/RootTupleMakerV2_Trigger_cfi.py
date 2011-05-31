9209f020623478024ab7f803c0304523b12e9fb9
santanas <>
2011-05-31 15:54:37 +0000

    updated triggers up to run 165980

--- python/RootTupleMakerV2_Trigger_cfi.py
+++ python/RootTupleMakerV2_Trigger_cfi.py
@@ -6,93 +6,162 @@ rootTupleTrigger = cms.EDProducer("RootTupleMakerV2_Trigger",
 
     # HLT config browser : http://j2eeps.cern.ch/cms-project-confdb-hltdev/browser/
     # HLT Tools https://twiki.cern.ch/twiki/bin/view/CMS/HLTriggerTools
+    # ID/Iso egamma definitions: https://twiki.cern.ch/twiki/bin/view/CMS/EgammaWorkingPointsv3
 
     HLTPathsOfInterest = cms.vstring(
                                      ##############
-                                     ## 2011: run range 160329-163876
+                                     ## 2011: run range 160329-165980
                                      ##############
 
-                                     ## Single Electrons ## 
+                                     #---------------------------------------------
+                                     ## SingleElectron ##
+                                     #---------------------------------------------
                                      # from /cdaq/physics/Run2011/5e32/v4.2/HLT/V1
                                      # 160404 <= run < 161216
                                      'HLT_Ele27_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v1', #0
                                      'HLT_Ele45_CaloIdVT_TrkIdT_v1', #1
-                                     'HLT_Ele90_NoSpikeFilter_v1', #2
                                      # from /cdaq/physics/Run2011/5e32/v6.1/HLT/V1
                                      # 161216 <= run < 163269
-                                     'HLT_Ele27_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v2', #3
-                                     'HLT_Ele32_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v1', #4
-                                     'HLT_Ele45_CaloIdVT_TrkIdT_v2', #5
-                                     'HLT_Ele90_NoSpikeFilter_v2', #6
+                                     'HLT_Ele27_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v2', #2
+                                     'HLT_Ele45_CaloIdVT_TrkIdT_v2', #3
                                      # from /cdaq/physics/Run2011/5e32/v8.1/HLT/V5
                                      # 163269 <= run < 163876
-                                     'HLT_Ele27_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v3', #7
-                                     'HLT_Ele32_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v2', #8
-                                     'HLT_Ele45_CaloIdVT_TrkIdT_v3', #9
-                                     'HLT_Ele90_NoSpikeFilter_v3', #10
+                                     'HLT_Ele27_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v3', #4
+                                     'HLT_Ele45_CaloIdVT_TrkIdT_v3', #5
+                                     #
+                                     # ==> nothing between 163876 and 165088 <==
+                                     #
+                                     # from /cdaq/physics/Run2011/1e33/v1.3/HLT/V2
+                                     # 165088 <= run < 165970
+                                     'HLT_Ele32_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v3', #6
+                                     'HLT_Ele52_CaloIdVT_TrkIdT_v1', #7
+                                     # from /cdaq/physics/Run2011/1e33/v2.3/HLT/V1
+                                     # 165970 <= run < 165980
+                                     'HLT_Ele32_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v4', #8 (prescaled)
+                                     'HLT_Ele42_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v1', #9
+                                     'HLT_Ele52_CaloIdVT_TrkIdT_v2', #10
+                                     'HLT_Ele25_WP80_PFMT40_v1', #11
 
-                                     ## Single Photons ##
+                                     #---------------------------------------------
+                                     ## DoubleElectron ##
+                                     #---------------------------------------------
                                      # from /cdaq/physics/Run2011/5e32/v4.2/HLT/V1
                                      # 160404 <= run < 161216
-                                     'HLT_Photon30_CaloIdVL_IsoL_v1', #11
-                                     'HLT_Photon30_CaloIdVL_v1', #12
-                                     'HLT_Photon75_CaloIdVL_IsoL_v1', #13
-                                     'HLT_Photon75_CaloIdVL_v1', #14
+                                     'HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL_v1', #12
                                      # from /cdaq/physics/Run2011/5e32/v6.1/HLT/V1
                                      # 161216 <= run < 163269
-                                     'HLT_Photon20_CaloIdVL_IsoL_v1', #15
-                                     'HLT_Photon30_CaloIdVL_IsoL_v2', #16
-                                     'HLT_Photon30_CaloIdVL_v2', #17
-                                     'HLT_Photon50_CaloIdVL_IsoL_v1', #18
-                                     'HLT_Photon75_CaloIdVL_IsoL_v2', #19
-                                     'HLT_Photon75_CaloIdVL_v2', #20
+                                     'HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL_v2', #13
                                      # from /cdaq/physics/Run2011/5e32/v8.1/HLT/V5
                                      # 163269 <= run < 163876
-                                     'HLT_Photon20_CaloIdVL_IsoL_v2', #21
-                                     'HLT_Photon30_CaloIdVL_IsoL_v3', #22
-                                     'HLT_Photon30_CaloIdVL_v3', #23
-                                     'HLT_Photon50_CaloIdVL_IsoL_v2', #24
-                                     'HLT_Photon75_CaloIdVL_IsoL_v3', #25
-                                     'HLT_Photon75_CaloIdVL_v3', #26
+                                     'HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL_v3', #14
+                                     #
+                                     # ==> nothing between 163876 and 165088 <==
+                                     #
+                                     # from /cdaq/physics/Run2011/1e33/v1.3/HLT/V2
+                                     # 165088 <= run < 165970
+                                     'HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL_v4', #15
+                                     # from /cdaq/physics/Run2011/1e33/v2.3/HLT/V1
+                                     # 165970 <= run < 165980
+                                     'HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL_v5', #16
+
+                                     #---------------------------------------------
+                                     ## ElectronHad ##
+                                     #---------------------------------------------
+                                     # from /cdaq/physics/Run2011/1e33/v1.3/HLT/V2
+                                     # 165088 <= run < 165970
+                                     'HLT_Ele17_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralJet30_CentralJet25_PFMHT15_v2', #17
+                                     'HLT_Ele17_CaloIdVT_TrkIdT_CentralJet30_CentralJet25_v1', #18 (prescaled)
+                                     # from /cdaq/physics/Run2011/1e33/v2.3/HLT/V1
+                                     # 165970 <= run < 165980
+                                     'HLT_Ele25_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralJet30_CentralJet25_PFMHT20_v4' #19
+                                     'HLT_Ele25_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralJet30_v1' #20 (prescaled)
+
+                                     #---------------------------------------------
+                                     ## Photon ##
+                                     #---------------------------------------------
+                                     # from /cdaq/physics/Run2011/5e32/v4.2/HLT/V1
+                                     # 160404 <= run < 161216
+                                     'HLT_Photon30_CaloIdVL_IsoL_v1', #21
+                                     'HLT_Photon30_CaloIdVL_v1', #22
+                                     'HLT_Photon75_CaloIdVL_IsoL_v1', #23
+                                     'HLT_Photon75_CaloIdVL_v1', #24
+                                     # from /cdaq/physics/Run2011/5e32/v6.1/HLT/V1
+                                     # 161216 <= run < 163269
+                                     'HLT_Photon20_CaloIdVL_IsoL_v1', #25
+                                     'HLT_Photon30_CaloIdVL_IsoL_v2', #26
+                                     'HLT_Photon30_CaloIdVL_v2', #27
+                                     'HLT_Photon50_CaloIdVL_IsoL_v1', #28
+                                     'HLT_Photon75_CaloIdVL_IsoL_v2', #29
+                                     'HLT_Photon75_CaloIdVL_v2', #30
+                                     # from /cdaq/physics/Run2011/5e32/v8.1/HLT/V5
+                                     # 163269 <= run < 163876
+                                     'HLT_Photon20_CaloIdVL_IsoL_v2', #31
+                                     'HLT_Photon30_CaloIdVL_IsoL_v3', #32
+                                     'HLT_Photon30_CaloIdVL_v3', #33
+                                     'HLT_Photon50_CaloIdVL_IsoL_v2', #34
+                                     'HLT_Photon75_CaloIdVL_IsoL_v3', #35
+                                     'HLT_Photon75_CaloIdVL_v3', #36
+                                     #
+                                     # ==> nothing between 163876 and 165088 <==
+                                     #
+                                     # from /cdaq/physics/Run2011/1e33/v1.3/HLT/V2
+                                     # 165088 <= run < 165970
+                                     'HLT_Photon20_CaloIdVL_IsoL_v3', #37
+                                     'HLT_Photon30_CaloIdVL_IsoL_v4', #38
+                                     'HLT_Photon30_CaloIdVL_v4', #39
+                                     'HLT_Photon50_CaloIdVL_IsoL_v3', #40
+                                     'HLT_Photon50_CaloIdVL_v1', #41
+                                     'HLT_Photon75_CaloIdVL_IsoL_v4', #42
+                                     'HLT_Photon75_CaloIdVL_v4', #43
+                                     'HLT_Photon90_CaloIdVL_IsoL_v1', #44
+                                     'HLT_Photon90_CaloIdVL_v1', #45
+                                     # from /cdaq/physics/Run2011/1e33/v2.3/HLT/V1
+                                     # 165970 <= run < 165980
+                                     'HLT_Photon20_CaloIdVL_IsoL_v4', #46
+                                     'HLT_Photon30_CaloIdVL_IsoL_v5', #47
+                                     'HLT_Photon30_CaloIdVL_v5', #48
+                                     'HLT_Photon50_CaloIdVL_IsoL_v4', #49
+                                     'HLT_Photon50_CaloIdVL_v2', #50
+                                     'HLT_Photon75_CaloIdVL_IsoL_v5', #51
+                                     'HLT_Photon75_CaloIdVL_v5', #52
+                                     'HLT_Photon90_CaloIdVL_IsoL_v2', #53
+                                     'HLT_Photon90_CaloIdVL_v2', #54
+
+                                     #---------------------------------------------
+                                     ## SingleMu ##
+                                     #---------------------------------------------
+                                     # from /cdaq/physics/Run2011/5e32/v4.2/HLT/V1
+                                     # 160404 <= run < 163269
+                                     'HLT_Mu20_v1', #55
+                                     'HLT_Mu24_v1', #56
+                                     'HLT_Mu30_v1', #57
+                                     # from /cdaq/physics/Run2011/5e32/v8.1/HLT/V5
+                                     # 163269 <= run < 163876
+                                     'HLT_Mu24_v2', #58
+                                     'HLT_Mu30_v2', #59
+                                     #
+                                     # ==> nothing between 163876 and 165088 <==
+                                     #
+                                     # from /cdaq/physics/Run2011/1e33/v1.3/HLT/V2
+                                     # 165088 <= run < 165980
+                                     'HLT_Mu30_v3', #60 
+                                     'HLT_Mu40_v1', #61
                                      
-                                     ## Double Photons ##
-                                     # from /cdaq/physics/Run2011/5e32/v4.2/HLT/V1
-                                     # 160404 <= run < 161216
-                                     'HLT_DoublePhoton33_v1', #27
-                                     'HLT_Photon26_CaloIdL_IsoVL_Photon18_v1', #28
-                                     'HLT_Photon26_IsoVL_Photon18_v1', #29
-                                     'HLT_Photon32_CaloIdL_Photon26_CaloIdL_v1', #30
-                                     # from /cdaq/physics/Run2011/5e32/v6.1/HLT/V1
-                                     # 161216 <= run < 163269
-                                     'HLT_DoublePhoton33_v2', #31
-                                     'HLT_Photon26_CaloIdL_IsoVL_Photon18_v2', #32
-                                     'HLT_Photon26_IsoVL_Photon18_v2', #33
-                                     'HLT_Photon32_CaloIdL_Photon26_CaloIdL_v2', #34
-                                     # from /cdaq/physics/Run2011/5e32/v8.1/HLT/V5
-                                     # 163269 <= run < 163876
-                                     'HLT_DoublePhoton33_v3', #35
-                                     'HLT_Photon26_CaloIdL_IsoVL_Photon18_v3', #36
-                                     'HLT_Photon26_IsoVL_Photon18_IsoVL_v3', #37
-                                     'HLT_Photon32_CaloIdL_Photon26_CaloIdL_v3', #38
-
-                                     ## Single Muons ##
+                                     #---------------------------------------------
+                                     ## DoubleMu ##
+                                     #---------------------------------------------
                                      # from /cdaq/physics/Run2011/5e32/v4.2/HLT/V1
                                      # 160404 <= run < 163269
-                                     'HLT_Mu20_v1', #39
-                                     'HLT_Mu24_v1', #40
-                                     'HLT_Mu30_v1', #41
+                                     'HLT_DoubleMu7_v1', #62
                                      # from /cdaq/physics/Run2011/5e32/v8.1/HLT/V5
                                      # 163269 <= run < 163876
-                                     'HLT_Mu24_v2', #42
-                                     'HLT_Mu30_v2', #43
-
-                                     ## Double Muons ##
-                                     # from /cdaq/physics/Run2011/5e32/v4.2/HLT/V1
-                                     # 160404 <= run < 163269
-                                     'HLT_DoubleMu7_v1', #44
-                                     # from /cdaq/physics/Run2011/5e32/v8.1/HLT/V5
-                                     # 163269 <= run < 163876
-                                     'HLT_DoubleMu7_v2', #45
+                                     'HLT_DoubleMu7_v2', #63
+                                     #
+                                     # ==> nothing between 163876 and 165088 <==
+                                     #
+                                     # from /cdaq/physics/Run2011/1e33/v1.3/HLT/V2
+                                     # 165088 <= run < 165980
+                                     'HLT_Mu13_Mu8_v2', #64
                                      )
 
 )

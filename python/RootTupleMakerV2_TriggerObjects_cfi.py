919fd23c6ef6ed06427a905935d8a33eab2a3229
eberry <>
2012-01-27 14:04:55 +0000

    Added filters for LQ1 T&P study

--- python/RootTupleMakerV2_TriggerObjects_cfi.py
+++ python/RootTupleMakerV2_TriggerObjects_cfi.py
@@ -1,10 +1,30 @@
 import FWCore.ParameterSet.Config as cms
 
-rootTupleTriggerObjectsSingleEleIDISO = cms.EDProducer("RootTupleMakerV2_TriggerObjects",
-    InputTag = cms.InputTag('hltTriggerSummaryAOD'),
-    FilterID = cms.InputTag('hltEle15CaloIdVTTrkIdTCaloIsoTTrkIsoTTrackIsolFilter','','HLT'),
-    Prefix = cms.string('HLTEle15IdIso'),
+rootTupleTriggerObjectsHLTEle32CaloIdTCaloIsoTTrkIdTTrkIsoTEle17TrackIsolFilter = cms.EDProducer("RootTupleMakerV2_TriggerObjects",
+    InputTag = cms.InputTag('hltTriggerSummaryAOD',"","HLT"),
+    FilterID = cms.InputTag("hltEle32CaloIdTCaloIsoTTrkIdTTrkIsoTEle17TrackIsolFilter","","HLT"),
+    Prefix = cms.string("HLTEle32CaloIdTCaloIsoTTrkIdTTrkIsoTEle17TrackIsolFilter"),
     Suffix = cms.string(''),
     MaxSize = cms.uint32(10)
 )
+
+rootTupleTriggerObjectsHLTEle32CaloIdTCaloIsoTTrkIdTTrkIsoTSC17HEDoubleFilter = cms.EDProducer("RootTupleMakerV2_TriggerObjects",
+    InputTag = cms.InputTag('hltTriggerSummaryAOD',"","HLT"),
+    FilterID = cms.InputTag("hltEle32CaloIdTCaloIsoTTrkIdTTrkIsoTSC17HEDoubleFilter","","HLT"),
+    Prefix = cms.string("HLTEle32CaloIdTCaloIsoTTrkIdTTrkIsoTSC17HEDoubleFilter"),
+    Suffix = cms.string(''),
+    MaxSize = cms.uint32(10)
+)
+
+rootTupleTriggerObjectsHLTEle27WP80TrackIsoFilter = cms.EDProducer("RootTupleMakerV2_TriggerObjects",
+    InputTag = cms.InputTag('hltTriggerSummaryAOD',"","HLT"),
+    FilterID = cms.InputTag("hltEle27WP80TrackIsoFilter","","HLT"),
+    Prefix = cms.string("HLTEle27WP80TrackIsoFilter"),
+    Suffix = cms.string(''),
+    MaxSize = cms.uint32(10)
+)
+
+
+
+
 

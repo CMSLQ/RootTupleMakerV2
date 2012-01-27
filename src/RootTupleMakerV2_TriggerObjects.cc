669c2cc77f9120a95bad78bc7376316332a9b006
eberry <>
2012-01-27 13:50:58 +0000

    Added new variables for LQ1 tag and probe studies

--- src/RootTupleMakerV2_TriggerObjects.cc
+++ src/RootTupleMakerV2_TriggerObjects.cc
@@ -11,17 +11,26 @@ RootTupleMakerV2_TriggerObjects::RootTupleMakerV2_TriggerObjects(const edm::Para
     suffix     (iConfig.getParameter<std::string>  ("Suffix")),
     maxSize    (iConfig.getParameter<unsigned int> ("MaxSize"))
 {
-  produces<std::vector<double> > ( prefix + "Pt"  + suffix );
-  produces<std::vector<double> > ( prefix + "Eta" + suffix );
-  produces<std::vector<double> > ( prefix + "Phi" + suffix );
+  produces<std::vector<int   > > ( prefix + "ID"     + suffix );
+  produces<std::vector<double> > ( prefix + "Pt"     + suffix );
+  produces<std::vector<double> > ( prefix + "Et"     + suffix );
+  produces<std::vector<double> > ( prefix + "Eta"    + suffix );
+  produces<std::vector<double> > ( prefix + "Phi"    + suffix );
+  produces<std::vector<double> > ( prefix + "Energy" + suffix );
+  produces<std::vector<double> > ( prefix + "Mass"   + suffix );
+
 }
 
 void RootTupleMakerV2_TriggerObjects::
 produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
-
-  std::auto_ptr<std::vector<double> > pt  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<double> > eta ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<double> > phi ( new std::vector<double>()  );
+  
+  std::auto_ptr<std::vector<int>    > id     ( new std::vector<int   >()  );
+  std::auto_ptr<std::vector<double> > pt     ( new std::vector<double>()  );
+  std::auto_ptr<std::vector<double> > et     ( new std::vector<double>()  );
+  std::auto_ptr<std::vector<double> > eta    ( new std::vector<double>()  );
+  std::auto_ptr<std::vector<double> > phi    ( new std::vector<double>()  );
+  std::auto_ptr<std::vector<double> > energy ( new std::vector<double>()  );
+  std::auto_ptr<std::vector<double> > mass   ( new std::vector<double>()  );
   
   edm::Handle<trigger::TriggerEvent> triggerEvent;
   iEvent.getByLabel( inputTag ,triggerEvent);
@@ -31,30 +40,33 @@ produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
     const trigger::TriggerObjectCollection & triggerObjects = triggerEvent -> getObjects();
     trigger::size_type filter_index = triggerEvent -> filterIndex ( filterID ) ;
     trigger::size_type n_filters    = triggerEvent -> sizeFilters();
-    trigger::size_type n_colls      = triggerEvent -> sizeCollections();
-
-    for (trigger::size_type i = 0; i < n_colls ; ++i) {
-      std::cout << triggerEvent -> collectionTag(i) << std::endl;
-    }
-
+    // trigger::size_type n_colls      = triggerEvent -> sizeCollections();
+    
     if ( filter_index < n_filters ) {
       const trigger::Keys & triggerKeys ( triggerEvent -> filterKeys ( filter_index ) );
       const int nkeys = triggerKeys.size();
 
       for (int ikey = 0; ikey < nkeys; ++ikey ) {
 	const trigger::TriggerObject& triggerObject = triggerObjects[ triggerKeys [ ikey ] ];
-	pt  -> push_back ( triggerObject.pt () );
-	eta -> push_back ( triggerObject.eta() );
-	phi -> push_back ( triggerObject.phi() );
+	id     -> push_back ( triggerObject.id    ());
+	pt     -> push_back ( triggerObject.pt    ());
+	et     -> push_back ( triggerObject.et    ());
+	eta    -> push_back ( triggerObject.eta   ());
+	phi    -> push_back ( triggerObject.phi   ());
+	energy -> push_back ( triggerObject.energy());
+	mass   -> push_back ( triggerObject.mass  ());
       }
     }
   } else { 
     edm::LogError("RootTupleMakerV2_TriggerObjectsError") << "Error! Can't get the product " << inputTag;
   }
 
-  
-  
-  iEvent.put( pt , prefix + "Pt"  + suffix );
-  iEvent.put( eta, prefix + "Eta" + suffix );
-  iEvent.put( phi, prefix + "Phi" + suffix );
+  iEvent.put( id    , prefix + "ID"     + suffix );
+  iEvent.put( pt    , prefix + "Pt"     + suffix );
+  iEvent.put( et    , prefix + "Et"     + suffix );
+  iEvent.put( eta   , prefix + "Eta"    + suffix );
+  iEvent.put( phi   , prefix + "Phi"    + suffix );
+  iEvent.put( energy, prefix + "Energy" + suffix );
+  iEvent.put( mass  , prefix + "Mass"   + suffix );
+
 }

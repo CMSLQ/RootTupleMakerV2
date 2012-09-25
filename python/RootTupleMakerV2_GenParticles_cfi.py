3b2634ca78300add3f869d082fc84effbe86a2da
hsaka <>
2012-09-25 22:39:08 +0000

    Added optional modules from LeptonJetGenTools.genTauMuElFromLQs_cfi and LeptonJetGenTools.genTauMuElFromZs_cfi

--- python/RootTupleMakerV2_GenParticles_cfi.py
+++ python/RootTupleMakerV2_GenParticles_cfi.py
@@ -21,3 +21,56 @@ rootTupleGenTausFromLQTops = cms.EDProducer("RootTupleMakerV2_GenParticles",
     MaxSize = cms.uint32(25)
 )
 
+# -------------------------------------------------------------------------------------------------------------- #
+# Optional modules                                                                                               #
+# See: Leptoquarks.LeptonJetGenTools.genTauMuElFromLQs_cfi , Leptoquarks.LeptonJetGenTools.genTauMuElFromZs_cfi  #
+# -------------------------------------------------------------------------------------------------------------- #
+
+#rootTupleGenMuonsFromLQTaus = cms.EDProducer("RootTupleMakerV2_GenParticles",
+#                                             InputTag = cms.InputTag('genMuonsFromLQTaus'),
+#                                             Prefix = cms.string('GenLQTauMuon'),
+#                                             Suffix = cms.string(''),
+#                                             MaxSize = cms.uint32(25)
+#                                             )
+
+#rootTupleGenElectronsFromLQTaus = cms.EDProducer("RootTupleMakerV2_GenParticles",
+#                                                 InputTag = cms.InputTag('genElectronsFromLQTaus'),
+#                                                 Prefix = cms.string('GenLQTauElectron'),
+#                                                 Suffix = cms.string(''),
+#                                                 MaxSize = cms.uint32(25)
+#                                                 )
+
+#rootTupleGenMuonsFromLQTops = cms.EDProducer("RootTupleMakerV2_GenParticles",
+#                                             InputTag = cms.InputTag('genMuonsFromLQTops'),
+#                                             Prefix = cms.string('GenLQTopXMuon'),
+#                                             Suffix = cms.string(''),
+#                                             MaxSize = cms.uint32(25)
+#                                             )
+
+#rootTupleGenElectronsFromLQTops = cms.EDProducer("RootTupleMakerV2_GenParticles",
+#                                                 InputTag = cms.InputTag('genElectronsFromLQTops'),
+#                                                 Prefix = cms.string('GenLQTopXElectron'),
+#                                                 Suffix = cms.string(''),
+#                                                 MaxSize = cms.uint32(25)
+#                                                 )
+
+#rootTupleGenTausFromZs = cms.EDProducer("RootTupleMakerV2_GenParticles",
+#                                        InputTag = cms.InputTag('genTausFromZs'),
+#                                        Prefix = cms.string('GenZTau'),
+#                                        Suffix = cms.string(''),
+#                                        MaxSize = cms.uint32(25)
+#                                        )
+
+#rootTupleGenMuonsFromZs = cms.EDProducer("RootTupleMakerV2_GenParticles",
+#                                         InputTag = cms.InputTag('genMuonsFromZs'),
+#                                         Prefix = cms.string('GenZMu'),
+#                                         Suffix = cms.string(''),
+#                                         MaxSize = cms.uint32(25)
+#                                         )
+
+#rootTupleGenElectronsFromZs = cms.EDProducer("RootTupleMakerV2_GenParticles",
+#                                             InputTag = cms.InputTag('genElectronsFromZs'),
+#                                             Prefix = cms.string('GenZElectron'),
+#                                             Suffix = cms.string(''),
+#                                             MaxSize = cms.uint32(25)
+#                                             )

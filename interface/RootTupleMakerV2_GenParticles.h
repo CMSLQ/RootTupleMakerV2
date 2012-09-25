66cbc404ae1c13076dd398ed56d5a55dfe2a7252
hsaka <>
2012-09-25 09:51:33 +0000

    Added visible momentum information for Taus

--- interface/RootTupleMakerV2_GenParticles.h
+++ interface/RootTupleMakerV2_GenParticles.h
@@ -9,8 +9,14 @@
 class RootTupleMakerV2_GenParticles : public edm::EDProducer {
  public:
   explicit RootTupleMakerV2_GenParticles(const edm::ParameterSet&);
-  int  getGenTauDecayMode(const reco::GenParticle*);
-  void countDecayProducts(const reco::GenParticle*,int&,int&,int&,int&,int&,int&,int&,int&);
+  //
+  //Tau Specific functions
+  reco::Candidate::LorentzVector getVisMomentum(const std::vector<const reco::GenParticle*>& , int );
+  reco::Candidate::LorentzVector getInvisMomentum(const std::vector<const reco::GenParticle*>& , int );
+  void  findDaughters(const reco::GenParticle*, std::vector<const reco::GenParticle*>&, int = -1);
+  bool  isNeutrino(const reco::GenParticle*);
+  int   getGenTauDecayMode(const reco::GenParticle*);
+  void  countDecayProducts(const reco::GenParticle*,int&,int&,int&,int&,int&,int&,int&,int&);
 
  private:
   void produce( edm::Event &, const edm::EventSetup & );
--- src/RootTupleMakerV2_GenParticles.cc
+++ src/RootTupleMakerV2_GenParticles.cc
@@ -1,7 +1,8 @@
 #include "Leptoquarks/RootTupleMakerV2/interface/RootTupleMakerV2_GenParticles.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "RecoTauTag/TauTagTools/interface/GeneratorTau.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/Framework/interface/Event.h"
 #include "TMath.h"
 
 RootTupleMakerV2_GenParticles::RootTupleMakerV2_GenParticles(const edm::ParameterSet& iConfig) :
@@ -26,6 +27,9 @@ RootTupleMakerV2_GenParticles::RootTupleMakerV2_GenParticles(const edm::Paramete
   produces <std::vector<int> >    ( prefix + "Status"       + suffix );
   produces <std::vector<int> >    ( prefix + "MotherIndex"  + suffix );
   produces <std::vector<int> >    ( prefix + "TauDecayMode" + suffix );
+  produces <std::vector<double> > ( prefix + "TauVisiblePt" + suffix );
+  produces <std::vector<double> > ( prefix + "TauVisibleEta"+ suffix );
+  produces <std::vector<double> > ( prefix + "TauVisiblePhi"+ suffix );
 }
 
 void RootTupleMakerV2_GenParticles::
@@ -33,22 +37,24 @@ produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
   std::auto_ptr<std::vector<double> >  eta  ( new std::vector<double>()  );
   std::auto_ptr<std::vector<double> >  phi  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<double> >  p  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<double> >  px  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<double> >  py  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<double> >  pz  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<double> >  pt  ( new std::vector<double>()  );
+  std::auto_ptr<std::vector<double> >  p    ( new std::vector<double>()  );
+  std::auto_ptr<std::vector<double> >  px   ( new std::vector<double>()  );
+  std::auto_ptr<std::vector<double> >  py   ( new std::vector<double>()  );
+  std::auto_ptr<std::vector<double> >  pz   ( new std::vector<double>()  );
+  std::auto_ptr<std::vector<double> >  pt   ( new std::vector<double>()  );
   std::auto_ptr<std::vector<double> >  energy  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<int> >     pdgId ( new std::vector<int>()  );
+  std::auto_ptr<std::vector<int> >     pdgId   ( new std::vector<int>()  );
   std::auto_ptr<std::vector<double> >  vx  ( new std::vector<double>()  );
   std::auto_ptr<std::vector<double> >  vy  ( new std::vector<double>()  );
   std::auto_ptr<std::vector<double> >  vz  ( new std::vector<double>()  );
   std::auto_ptr<std::vector<int> >     numDaught  ( new std::vector<int>()  );
-  std::auto_ptr<std::vector<int> >     status  ( new std::vector<int>()  );
-  std::auto_ptr<std::vector<int> >     motherIndex  ( new std::vector<int>()  );
-  std::auto_ptr<std::vector<int> >     taudecaymode ( new std::vector<int>()  );
+  std::auto_ptr<std::vector<int> >     status     ( new std::vector<int>()  );
+  std::auto_ptr<std::vector<int> >     motherIndex   ( new std::vector<int>()  );
+  std::auto_ptr<std::vector<int> >     taudecaymode  ( new std::vector<int>()  );
+  std::auto_ptr<std::vector<double> >  tauvisiblept  ( new std::vector<double>()  );
+  std::auto_ptr<std::vector<double> >  tauvisibleeta ( new std::vector<double>()  );
+  std::auto_ptr<std::vector<double> >  tauvisiblephi ( new std::vector<double>()  );
   
-  //-----------------------------------------------------------------
   if( !iEvent.isRealData() ) {
     edm::Handle<reco::GenParticleCollection> genParticles;
     iEvent.getByLabel(inputTag, genParticles);
@@ -76,12 +82,31 @@ produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
         vz->push_back( it->vz() );
         numDaught->push_back( it->numberOfDaughters() );
         status->push_back( it->status() );
-	//---------
-	//if gen-particle is a tau, check decay mode:
-	int getGenTauDecayMode_ = 0;
-	if( abs(it->pdgId()) == 15 ) getGenTauDecayMode_ =  getGenTauDecayMode( & (*it) );
-	taudecaymode->push_back( (int)(getGenTauDecayMode_) );
-	//---------
+
+	// >>>>>>  if gen-particle is a tau, check decay mode and fill-in visible momentum parameters:
+	int getGenTauDecayMode_ = 0; double tauVisPt  = -999.0; double tauVisEta = -999.0; double tauVisPhi = -999.0;
+	if( abs(it->pdgId()) == 15 ){
+	  getGenTauDecayMode_ =  getGenTauDecayMode( & (*it) );
+	  if( getGenTauDecayMode_>0 ){//get visible momentum only if decay mode is determined
+	    std::vector<const reco::GenParticle*> TauDaughters;
+	    findDaughters( & (*it), TauDaughters, 1);
+	    LorentzVector tauVis   = getVisMomentum( TauDaughters, 1);
+	    tauVisPt = tauVis.pt(); tauVisEta = tauVis.eta(); tauVisPhi = tauVis.phi();
+	    // ------------------------------------------------------------------ DEBUG OUTPUT --------------------------------------------------------------//
+	    //LorentzVector tauInvis = getInvisMomentum( TauDaughters, 1);
+	    //std::cout<< "Tau DecayMode/NoOfDaughters: "<<getGenTauDecayMode_<<" / "<<TauDaughters.size()<<std::endl;
+	    //std::cout<< "          visiblePt/Eta/Phi: "<<tauVisPt<<" / "<<tauVisEta<<" / "<<tauVisPhi <<std::endl;
+	    //std::cout<< "        invisiblePt/Eta/Phi: "<<tauInvis.pt()<<" / "<<tauInvis.eta()<<" / "<<tauInvis.phi() <<std::endl;
+	    //std::cout<< "visible+invisiblePt/Eta/Phi: "<<(tauVis+tauInvis).pt()<<" / "<<(tauVis+tauInvis).eta()<<" / "<<(tauVis+tauInvis).phi() <<std::endl;
+	    //std::cout<< "              genPt/Eta/Phi: "<<it->pt()<<" / "<< it->eta()<<" / "<<it->phi()<<std::endl;
+	    // ------------------------------------------------------------------ ------------ --------------------------------------------------------------//
+	  }
+	}
+	taudecaymode -> push_back( (int)(getGenTauDecayMode_) );
+	tauvisiblept -> push_back( tauVisPt  );
+	tauvisibleeta-> push_back( tauVisEta );
+	tauvisiblephi-> push_back( tauVisPhi );
+	// <<<<<<
 
 	int idx = -1;
         for( reco::GenParticleCollection::const_iterator mit = genParticles->begin(); mit != genParticles->end(); ++mit ) {
@@ -96,31 +121,79 @@ produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
       edm::LogError("RootTupleMakerV2_GenParticlesError") << "Error! Can't get the product " << inputTag;
     }
   }
-  //-----------------------------------------------------------------
+
   // put vectors in the event
-  iEvent.put( eta,         prefix + "Eta"         + suffix );
-  iEvent.put( phi,         prefix + "Phi"         + suffix );
-  iEvent.put( p,           prefix + "P"           + suffix );
-  iEvent.put( px,          prefix + "Px"          + suffix );
-  iEvent.put( py,          prefix + "Py"          + suffix );
-  iEvent.put( pz,          prefix + "Pz"          + suffix );
-  iEvent.put( pt,          prefix + "Pt"          + suffix );
-  iEvent.put( energy,      prefix + "Energy"      + suffix );
-  iEvent.put( pdgId,       prefix + "PdgId"       + suffix );
-  iEvent.put( vx,          prefix + "VX"          + suffix );
-  iEvent.put( vy,          prefix + "VY"          + suffix );
-  iEvent.put( vz,          prefix + "VZ"          + suffix );
-  iEvent.put( numDaught,   prefix + "NumDaught"   + suffix );
-  iEvent.put( status,      prefix + "Status"      + suffix );
-  iEvent.put( motherIndex, prefix + "MotherIndex" + suffix );
-  iEvent.put( taudecaymode,prefix + "TauDecayMode"+ suffix );
+  iEvent.put( eta,          prefix + "Eta"          + suffix );
+  iEvent.put( phi,          prefix + "Phi"          + suffix );
+  iEvent.put( p,            prefix + "P"            + suffix );
+  iEvent.put( px,           prefix + "Px"           + suffix );
+  iEvent.put( py,           prefix + "Py"           + suffix );
+  iEvent.put( pz,           prefix + "Pz"           + suffix );
+  iEvent.put( pt,           prefix + "Pt"           + suffix );
+  iEvent.put( energy,       prefix + "Energy"       + suffix );
+  iEvent.put( pdgId,        prefix + "PdgId"        + suffix );
+  iEvent.put( vx,           prefix + "VX"           + suffix );
+  iEvent.put( vy,           prefix + "VY"           + suffix );
+  iEvent.put( vz,           prefix + "VZ"           + suffix );
+  iEvent.put( numDaught,    prefix + "NumDaught"    + suffix );
+  iEvent.put( status,       prefix + "Status"       + suffix );
+  iEvent.put( motherIndex,  prefix + "MotherIndex"  + suffix );
+  iEvent.put( taudecaymode, prefix + "TauDecayMode" + suffix );
+  iEvent.put( tauvisiblept, prefix + "TauVisiblePt" + suffix );
+  iEvent.put( tauvisibleeta,prefix + "TauVisibleEta"+ suffix );
+  iEvent.put( tauvisiblephi,prefix + "TauVisiblePhi"+ suffix );
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------//
-// See: https://hypernews.cern.ch/HyperNews/CMS/get/tauid/301/1/1/1.html                                                               //
-// countDecayProducts()  and  getGenTauDecayMode()  are reproduced from:                                                               //
-// http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/TauAnalysis/CandidateTools/src/candidateAuxFunctions.cc?revision=1.26&view=markup  //
-//-------------------------------------------------------------------------------------------------------------------------------------//
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------//
+// See: https://hypernews.cern.ch/HyperNews/CMS/get/tauid/301/1/1/1.html                                                                    //
+// getVisMomentum(), getInvisMomentum(), findDaughters(), isNeutrino(), countDecayProducts() and getGenTauDecayMode() are reproduced from:  //
+// http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/TauAnalysis/CandidateTools/src/candidateAuxFunctions.cc?revision=1.26&view=markup       //
+//------------------------------------------------------------------------------------------------------------------------------------------//
+
+reco::Candidate::LorentzVector RootTupleMakerV2_GenParticles::getVisMomentum(const std::vector<const reco::GenParticle*>& daughters, int status){
+  reco::Candidate::LorentzVector p4Vis(0,0,0,0);
+  for ( std::vector<const reco::GenParticle*>::const_iterator daughter = daughters.begin();
+	daughter != daughters.end(); ++daughter ) {
+    if ( (status == -1 || (*daughter)->status() == status) && !isNeutrino(*daughter) ) {
+      // ------ debug ------
+      //std::cout << "adding daughter: pdgId = " << (*daughter)->pdgId() << ", Pt = " << (*daughter)->pt() << ","
+      //  << " eta = " << (*daughter)->eta() << ", phi = " << (*daughter)->phi()*180./TMath::Pi() << std::endl;
+      // ------ ----- ------
+      p4Vis += (*daughter)->p4();
+    }
+  }
+  // ------ debug ------
+  //std::cout << "--> vis. Momentum: Pt = " << p4Vis.pt() << ", eta = " << p4Vis.eta() << ", phi = " << p4Vis.phi() << std::endl;
+  // ------ ----- ------
+  return p4Vis;
+}
+
+reco::Candidate::LorentzVector RootTupleMakerV2_GenParticles::getInvisMomentum(const std::vector<const reco::GenParticle*>& daughters, int status){
+  reco::Candidate::LorentzVector p4Invis(0,0,0,0);
+  for ( std::vector<const reco::GenParticle*>::const_iterator daughter = daughters.begin();
+	daughter != daughters.end(); ++daughter ) {
+    if ( (status == -1 || (*daughter)->status() == status) && isNeutrino(*daughter) ) {
+      p4Invis += (*daughter)->p4();
+    }
+  }
+  return p4Invis;
+}
+
+void RootTupleMakerV2_GenParticles::findDaughters(const reco::GenParticle* mother,  std::vector<const reco::GenParticle*>& daughters, int status){
+  unsigned numDaughters = mother->numberOfDaughters();
+  for ( unsigned iDaughter = 0; iDaughter < numDaughters; ++iDaughter ) {
+    const reco::GenParticle* daughter = mother->daughterRef(iDaughter).get();
+    if ( status == -1 || daughter->status() == status ) daughters.push_back(daughter);
+    findDaughters(daughter, daughters, status);
+  }
+}
+
+bool RootTupleMakerV2_GenParticles::isNeutrino(const reco::GenParticle* daughter){
+  return ( TMath::Abs(daughter->pdgId()) == 12 || TMath::Abs(daughter->pdgId()) == 14 || TMath::Abs(daughter->pdgId()) == 16 );
+}
+
 void RootTupleMakerV2_GenParticles::countDecayProducts(const reco::GenParticle* genParticle,
 						       int& numElectrons, int& numElecNeutrinos, int& numMuons, int& numMuNeutrinos, 
 						       int& numChargedHadrons, int& numPi0s, int& numOtherNeutralHadrons, int& numPhotons)

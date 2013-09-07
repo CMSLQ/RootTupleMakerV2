7c2a75543aab03c88d009828732a87a172a560f9
eberry <>
2013-09-07 23:57:51 +0000

    Added MET collections for smeared jets and for unclustered energy systematics

--- python/RootTupleMakerV2_MET_cfi.py
+++ python/RootTupleMakerV2_MET_cfi.py
@@ -30,7 +30,6 @@ rootTupleCaloMETType1Cor = cms.EDProducer("RootTupleMakerV2_MET",
     Suffix = cms.string('Type1Cor'),
     StoreUncorrectedMET = cms.bool(True),
     StoreMETSignificance = cms.bool(False)
-
 )
 
 rootTuplePFMETType1Cor = cms.EDProducer("RootTupleMakerV2_MET",
@@ -56,3 +55,28 @@ rootTuplePFMETType01XYCor = cms.EDProducer("RootTupleMakerV2_MET",
     StoreUncorrectedMET = cms.bool(False),
     StoreMETSignificance = cms.bool(True)
 )
+
+rootTuplePFMETType01XYCorJetSmeared = cms.EDProducer("RootTupleMakerV2_MET",
+    InputTag = cms.InputTag('patType1CorrectedPFMet'),
+    Prefix = cms.string('PF'),
+    Suffix = cms.string('Type01XYCorJetSmeared'),
+    StoreUncorrectedMET = cms.bool(False),
+    StoreMETSignificance = cms.bool(True)
+)
+
+rootTuplePFMETType01XYCorJetSmearedUnclusteredUp = cms.EDProducer("RootTupleMakerV2_MET",
+    InputTag = cms.InputTag('patType1CorrectedPFMetUnclusteredEnUp'),
+    Prefix = cms.string('PF'),
+    Suffix = cms.string('Type01XYCorJetSmearedUnclusteredUp'),
+    StoreUncorrectedMET = cms.bool(False),
+    StoreMETSignificance = cms.bool(False)
+)
+
+rootTuplePFMETType01XYCorJetSmearedUnclusteredDown = cms.EDProducer("RootTupleMakerV2_MET",
+    InputTag = cms.InputTag('patType1CorrectedPFMetUnclusteredEnDown'),
+    Prefix = cms.string('PF'),
+    Suffix = cms.string('Type01XYCorJetSmearedUnclusteredDown'),
+    StoreUncorrectedMET = cms.bool(False),
+    StoreMETSignificance = cms.bool(False)
+)
+

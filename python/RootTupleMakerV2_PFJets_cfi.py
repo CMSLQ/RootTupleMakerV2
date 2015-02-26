7a33f5c2909362e1cd05e98eeef2cca644c67e4c
Seth Cooper <seth.cooper@cern.ch>
2015-02-26 10:16:28 +0100

    Fix jets/vertex input tags for miniAOD; ignore smeared jet collections for now.

--- python/RootTupleMakerV2_PFJets_cfi.py
+++ python/RootTupleMakerV2_PFJets_cfi.py
@@ -1,16 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
 rootTuplePFJets = cms.EDProducer("RootTupleMakerV2_PFJets",
-    InputTag = cms.InputTag('selectedPatJetsAK5PF'),
-    # InputTagL1Offset    = cms.InputTag('selectedPatJetsAK5PFL1Offset'),
-    InputTagSmearedUp   = cms.InputTag('smearedPatJetsAK5PFresUp'),                                 
-    InputTagSmearedDown = cms.InputTag('smearedPatJetsAK5PFresDown'),                                 
-    InputTagScaledUp    = cms.InputTag('shiftedPatJetsAK5PFenUpForCorrMEt'),                                 
-    InputTagScaledDown  = cms.InputTag('shiftedPatJetsAK5PFenDownForCorrMEt'),                                 
+    InputTag = cms.InputTag('slimmedJets'),
+    ## InputTagL1Offset    = cms.InputTag('selectedPatJetsAK5PFL1Offset'),
+    #InputTagSmearedUp   = cms.InputTag('smearedPatJetsAK5PFresUp'),                                 
+    #InputTagSmearedDown = cms.InputTag('smearedPatJetsAK5PFresDown'),                                 
+    #InputTagScaledUp    = cms.InputTag('shiftedPatJetsAK5PFenUpForCorrMEt'),                                 
+    #InputTagScaledDown  = cms.InputTag('shiftedPatJetsAK5PFenDownForCorrMEt'),                                 
     Prefix = cms.string('PFJet'),
     Suffix = cms.string(''),
     MaxSize = cms.uint32(30),
     JECUncertainty = cms.string('AK5PF'),
     ReadJECuncertainty = cms.bool(True),
-    VertexInputTag = cms.InputTag('offlinePrimaryVertices')
+    VertexInputTag = cms.InputTag('offlineSlimmedPrimaryVertices')
 )

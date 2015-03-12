6d4e667d5db33fb40cb1ae997e8fe68db195997d
Seth Cooper <seth.cooper@cern.ch>
2015-03-12 11:22:25 +0100

    Fix indents; attempt trigger matching (didn't work).

--- python/RootTupleMakerV2_PFJets_cfi.py
+++ python/RootTupleMakerV2_PFJets_cfi.py
@@ -1,7 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 rootTuplePFJets = cms.EDProducer("RootTupleMakerV2_PFJets",
-    InputTag = cms.InputTag('slimmedJets'),
+    InputTag = cms.InputTag('patJetsAK5'),
+    #InputTag = cms.InputTag('pfjetTriggerMatchEmbedderHLTEleJetJet'),
     ## InputTagL1Offset    = cms.InputTag('selectedPatJetsAK5PFL1Offset'),
     #InputTagSmearedUp   = cms.InputTag('smearedPatJetsAK5PFresUp'),                                 
     #InputTagSmearedDown = cms.InputTag('smearedPatJetsAK5PFresDown'),                                 
@@ -13,4 +14,29 @@ rootTuplePFJets = cms.EDProducer("RootTupleMakerV2_PFJets",
     JECUncertainty = cms.string('AK5PF'),
     ReadJECuncertainty = cms.bool(True),
     VertexInputTag = cms.InputTag('offlineSlimmedPrimaryVertices')
+)
+
+# Trigger matching
+#FIXME? For now we just match to the path of interest
+# Maybe later we can do more general matching
+# This path seems strange in terms of saved jets, though.
+# cross trigger strangeness?
+# Maybe this HN post could help: https://hypernews.cern.ch/HyperNews/CMS/get/physTools/2372/1/1/2.html
+pfjetTriggerMatchHLTEleJetJet = cms.EDProducer(
+  "PATTriggerMatcherDRLessByR"
+, src     = cms.InputTag( 'slimmedJets' )
+, matched = cms.InputTag( 'unpackedPatTrigger' )          
+, matchedCuts = cms.string( 'path ( "HLT_Ele45_CaloIdVT_GsfTrkIdT_PFJet200_PFJet50_v*" )' )
+#, matchedCuts = cms.string( 'type("TriggerPhoton") || type("TriggerElectron")' )
+, maxDeltaR = cms.double( 0.5 )
+, resolveAmbiguities    = cms.bool( True  )        # only one match per trigger object
+, resolveByMatchQuality = cms.bool( True  )        # take best match found per reco object: by DeltaR here (s. above)
+)
+
+pfjetTriggerMatchEmbedderHLTEleJetJet = cms.EDProducer(
+  "PATTriggerMatchElectronEmbedder"
+, src = cms.InputTag( 'slimmedJets' )
+, matches = cms.VInputTag(
+  cms.InputTag( 'pfjetTriggerMatchHLTEleJetJet' )
+  )
 )

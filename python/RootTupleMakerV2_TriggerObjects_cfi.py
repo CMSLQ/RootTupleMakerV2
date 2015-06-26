79fdc92c0fe88319525525f72e769089677b6b94
Seth Cooper <seth.cooper@cern.ch>
2015-06-26 09:59:19 +0200

    Change to HL prefix.

--- python/RootTupleMakerV2_TriggerObjects_cfi.py
+++ python/RootTupleMakerV2_TriggerObjects_cfi.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 rootTupleTriggerObjects= cms.EDProducer("RootTupleMakerV2_TriggerObjects",
     TriggerBitsTag = cms.InputTag("TriggerResults","","HLT"),
     TriggerObjectsTag = cms.InputTag("selectedPatTrigger"),
-    Prefix = cms.string("HLT"),
+    #Prefix = cms.string("HLT"),
+    Prefix = cms.string("HL"),
     Suffix = cms.string('')
 )
 

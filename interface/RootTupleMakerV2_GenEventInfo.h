461443b17a4eba4422707ee8156593cf66e02b14
David <dnash@cern.ch>
2015-08-19 21:03:04 +0200

    Added functionality for renorm./fact. scales

--- interface/RootTupleMakerV2_GenEventInfo.h
+++ interface/RootTupleMakerV2_GenEventInfo.h
@@ -5,12 +5,20 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+
 class RootTupleMakerV2_GenEventInfo : public edm::EDProducer {
  public:
   explicit RootTupleMakerV2_GenEventInfo(const edm::ParameterSet&);
 
+
  private:
   void produce( edm::Event &, const edm::EventSetup & );
+  virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
   const edm::InputTag   genEvtInfoInputTag;
   const bool            storePDFWeights;
   const edm::InputTag   pdfCTEQWeightsInputTag;

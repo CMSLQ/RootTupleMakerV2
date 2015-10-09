14234b2ec76175d138e5e1471b1ccc10a5e2d825
Seth Cooper <seth.cooper@cern.ch>
2015-10-09 17:55:46 +0200

    Update remake of AK5 PF jets.

--- python/ak5pfjets_cfi.py
+++ python/ak5pfjets_cfi.py
@@ -3,44 +3,17 @@ import FWCore.ParameterSet.Config as cms
 # Remake AK5 jets from packedPFcandidates
 # See: https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookMiniAOD#Advanced_topics_re_clustering_ev
 
-from RecoJets.JetProducers.ak5PFJets_cfi import ak5PFJets
+## Filter out neutrinos from packed GenParticles
+packedGenParticlesForJetsNoNu = cms.EDFilter("CandPtrSelector", src = cms.InputTag("packedGenParticles"), cut = cms.string("abs(pdgId) != 12 && abs(pdgId) != 14 && abs(pdgId) != 16"))
+## Define GenJets
 from RecoJets.JetProducers.ak5GenJets_cfi import ak5GenJets
-from RecoMET.METProducers.PFMET_cfi import pfMet
+ak5GenJetsNoNu = ak5GenJets.clone(src = 'packedGenParticlesForJetsNoNu')
 
-# Select candidates that would pass CHS requirements
-chs = cms.EDFilter("CandPtrSelector", src = cms.InputTag("packedPFCandidates"), cut = cms.string("fromPV"))
+## Select charged hadron subtracted packed PF candidates
+pfCHS = cms.EDFilter("CandPtrSelector", src = cms.InputTag("packedPFCandidates"), cut = cms.string("fromPV"))
+from RecoJets.JetProducers.ak5PFJets_cfi import ak5PFJets
+## Define AK5PFJetsCHS
+ak5PFJetsCHS = ak5PFJets.clone(src = 'pfCHS', doAreaFastjet = True)
+## Define AK5PFJets
+ak5PFJets = ak5PFJets.clone(src = 'packedPFCandidates')
 
-#makes chs ak5 jets   (instead of ak4 that are default in miniAOD 70X)
-ak5PFJetsCHS = ak5PFJets.clone(src = 'chs')
-#let's also make non-chs ak5 jets
-ak5PFJets = ak5PFJets.clone(src = 'packedPFCandidates') 
-
-# if we really want to study the ak5 vs ak4 we may need to remake gen Jets in ak5 too
-# in order to do so we use the "all status 1" gen particles (packedGenParticles)
-ak5GenJets = ak5GenJets.clone(src = 'packedGenParticles')
-
-# the following part is needed if you want to run b-tagging on the freshly made jets
-# CAVEAT: it is not 100% the same b-tagging as in RECO, but performance plots are almost identical
-
-# As tracks are not stored in miniAOD, and b-tag fwk for CMSSW < 72X does not accept candidates
-# we need to recreate tracks and pv for btagging in standard reco format:
-from RecoBTag.Configuration.RecoBTag_cff import *
-from RecoJets.Configuration.RecoJetAssociations_cff import *
-from RecoJets.JetAssociationProducers.ak5JTA_cff import *
-from PhysicsTools.PatAlgos.slimming.unpackedTracksAndVertices_cfi import *
-##DMM FIXME removed because of following error in 744:
-##--------
-##Exception Message:
-##two EventSetup Producers want to deliver type="CaloSubdetectorGeometry" label="ZDC"
-## from record ZDCGeometryRecord. The two providers are 
-##1) type="ZdcGeometryFromDBEP" label=""
-##2) type="ZdcHardcodeGeometryEP" label=""
-##--------
-#from Configuration.StandardSequences.Geometry_cff import *
-#from Configuration.StandardSequences.MagneticField_38T_cff import *
-
-ak5JetTracksAssociatorAtVertexPF.jets = cms.InputTag("ak5PFJetsCHS")
-ak5JetTracksAssociatorAtVertexPF.tracks = cms.InputTag("unpackedTracksAndVertices")
-impactParameterTagInfos.primaryVertex = cms.InputTag("unpackedTracksAndVertices")
-inclusiveSecondaryVertexFinderTagInfos.extSVCollection = cms.InputTag("unpackedTracksAndVertices","secondary","")
-#combinedSecondaryVertex.trackMultiplicityMin = 1#DMM FIXME removed to get running in 744....

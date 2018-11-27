c1277b768b5207147129c4827dfc6dfe68eb4b33
Seth Cooper <seth.cooper@cern.ch>
2018-11-27 19:01:44 +0100

    use HLT trigger results and unpackedPatTrigger objects

--- python/RootTupleMakerV2_TriggerObjects_cfi.py
+++ python/RootTupleMakerV2_TriggerObjects_cfi.py
@@ -2,8 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 rootTupleTriggerObjects= cms.EDProducer("RootTupleMakerV2_TriggerObjects",
     TriggerBitsTag = cms.InputTag("TriggerResults","","HLT"),
-    TriggerObjectsTag = cms.InputTag("selectedPatTrigger"),
-    #Prefix = cms.string("HLT"),
+    TriggerObjectsTag = cms.InputTag("unpackedPatTrigger"),
     Prefix = cms.string("HL"),
     Suffix = cms.string('')
 )

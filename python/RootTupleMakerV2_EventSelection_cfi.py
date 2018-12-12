7f88febee366f9199fae44b8e699220e4a7656f8
Seth Cooper <seth.cooper@cern.ch>
2018-12-12 14:30:22 +0100

    have to read the right triggerresults to get the filter flags

--- python/RootTupleMakerV2_EventSelection_cfi.py
+++ python/RootTupleMakerV2_EventSelection_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 rootTupleEventSelection = cms.EDProducer("RootTupleMakerV2_EventSelection",
     L1InputTag  = cms.InputTag('gtDigis'),
-    FilterResultsInputTag = cms.InputTag('TriggerResults','','HLT'),
+    FilterResultsInputTag = cms.InputTag('TriggerResults','','PAT'),
     # process label can sometimes be RECO and sometimes PAT
     # just get most recently produced collection
     # https://hypernews.cern.ch/HyperNews/CMS/get/physTools/3396/1/1/2.html

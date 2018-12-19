74df4eb81eac54cde938bbf652966c3b0e032e87
Seth Cooper <seth.cooper@cern.ch>
2018-12-19 10:43:10 +0100

    get more specific on electron/photon trigger paths to keep

--- python/RootTupleMakerV2_Trigger_cfi.py
+++ python/RootTupleMakerV2_Trigger_cfi.py
@@ -9,10 +9,32 @@ rootTupleTrigger = cms.EDProducer("RootTupleMakerV2_Trigger",
     # HLT Tools https://twiki.cern.ch/twiki/bin/view/CMS/HLTriggerTools
     # ID/Iso egamma definitions: https://twiki.cern.ch/twiki/bin/view/CMS/EgammaWorkingPointsv3
 
+    # keep if the full HLT path name contains any of these substrings
     HLTPathsOfInterest = cms.vstring(
-        'HLT_Ele',
+        # Electron
+        'HLT_Ele27',
+        'HLT_Ele32',
+        'HLT_Ele35',
+        'HLT_Ele40',
+        'HLT_Ele45',
+        'HLT_Ele105',
+        'HLT_Ele115',
+        # Photon
+        'HLT_Photon20',
+        'HLT_Photon22',
+        'HLT_Photon25',
+        'HLT_Photon30',
+        'HLT_Photon33',
+        'HLT_Photon36',
+        'HLT_Photon50',
+        'HLT_Photon75',
+        'HLT_Photon90',
+        'HLT_Photon120',
+        'HLT_Photon150',
+        'HLT_Photon175',
+        'HLT_Photon200',
+        # Mu
         'HLT_Mu',
-        'HLT_Photon',
     )
                                      
 )
